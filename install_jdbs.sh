1f19098bf951606d63ddc590c50c7ab7f81a24fb
immanuel-rajadurai <91907788+immanuel-rajadurai@users.noreply.github.com>
2024-10-29 16:09:43 +0000

    Update install_jdbs.sh

--- install_jdbs.sh
+++ install_jdbs.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# Download the MySQL JDBC Driver
 echo "Downloading MySQL JDBC Driver..."
 wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.33.zip -O mysql-connector-java.zip
 
-# Unzip the file
 echo "Extracting MySQL JDBC Driver..."
 unzip mysql-connector-java.zip
 
-# Move the JAR to the current directory
 echo "Moving JDBC Driver to current directory..."
 mv mysql-connector-java-8.0.33/mysql-connector-java-8.0.33.jar .
 
-# Clean up
 echo "Cleaning up..."
 rm -rf mysql-connector-java.zip mysql-connector-java-8.0.33/
 
